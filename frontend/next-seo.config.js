c4db97c1648153c1c6e53c6c278e72acb4b2596f
luke-h1 <luke.howsam@yahoo.com>
2021-02-18 19:38:29 +0000

    initial pass at setting up frontend dependencies

---
+++ frontend/next-seo.config.js
@@ -0,0 +1,17 @@
+const title = 'takemynotes.com';
+const description = 'Full stack note taking app';
+
+const SEO = {
+  title,
+  description,
+  canonical: 'https://take-my-notes.com',
+  openGraph: {
+    type: 'website',
+    locale: 'en_GB',
+    url: 'https://take-my-notes.com',
+    title,
+    description,
+  },
+};
+
+export default SEO;
