e14507b59e4fc7745632bfaf7689ea8e569d70f3
luke-h1 <luke.howsam@yahoo.com>
2021-02-19 13:47:20 +0000

    add sample note data

---
+++ backend/data/notes.js
@@ -0,0 +1,19 @@
+const notes = [
+  {
+    title: 'Take the trash out',
+    body: 'Note: Take the trash out',
+  },
+  {
+    title: 'Learn react',
+    body: 'Learn more react ⚡️',
+  },
+  {
+    title: 'Fix dinner',
+    body: 'cook dinner',
+  },
+  {
+    title: 'Make coffee',
+    body: 'Make some cofee!',
+  },
+
+];
