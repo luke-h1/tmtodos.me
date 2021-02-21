a08156f7c6ace746aa674023a01c7b243611691c
luke-h1 <luke.howsam@yahoo.com>
2021-02-21 11:51:15 +0000

    sort out error, msg & loader components

--- frontend/components/Error.tsx
+++ frontend/components/Error.tsx
@@ -11,17 +11,17 @@ interface Iprops {
   children: string;
 }
 
-const Message: React.FC<Iprops> = ({ children }) => {
+const Error: React.FC<Iprops> = ({ children }) => {
   return (
     <>
       <Flex justifyItems="center" justifyContent="center" direction="column" align="center">
-        <Alert status="success" w={200} align="center" borderRadius={10}>
+        <Alert status="error" w={200} align="center" borderRadius={10}>
           <AlertIcon />
-          <AlertTitle mr={2}>Message:</AlertTitle>
+          <AlertTitle mr={2}>Error:</AlertTitle>
           <AlertDescription color="black">{children}</AlertDescription>
         </Alert>
       </Flex>
     </>
   );
 };
-export default Message;
+export default Error;
