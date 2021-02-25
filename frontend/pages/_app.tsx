afe5f86f5a062a3249cd49ec38a7de7c39c0102b
luke-h1 <luke.howsam@yahoo.com>
2021-02-25 22:13:21 +0000

    remove redux prototype

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,19 +1,14 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import { Provider } from 'react-redux';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import Header from 'components/Header/Header';
-import store from '../store';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Provider store={store}>
-        <ChakraProvider>
-          <Header />
-          <Component {...pageProps} />
-        </ChakraProvider>
-      </Provider>
+      <ChakraProvider>
+        <Header />
+        <Component {...pageProps} />
+      </ChakraProvider>
     </>
   );
 }
