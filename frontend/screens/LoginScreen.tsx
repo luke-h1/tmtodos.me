82a02af87b7c83a454a9519bcddea17c8a42f472
luke-h1 <luke.howsam@yahoo.com>
2021-02-25 22:43:47 +0000

    start on login form

--- frontend/screens/LoginScreen.tsx
+++ frontend/screens/LoginScreen.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import { Box, Flex, Heading } from '@chakra-ui/react';
 
 const LoginScreen = () => (
   <>
-    <h1>Login page</h1>
+    <Flex direction="column" justify="center" align="center" mb="8">
+      <Box>
+        <Heading as="h1" fontSize="40px" mb={4}>
+          Login
+        </Heading>
+      </Box>
+    </Flex>
   </>
 );
 export default LoginScreen;
