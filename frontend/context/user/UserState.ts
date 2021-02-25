86a1b7a2e01691d1db82d82d5a7549e0f33d9b26
luke-h1 <luke.howsam@yahoo.com>
2021-02-25 20:21:37 +0000

    start on UserState

--- frontend/context/user/UserState.ts
+++ frontend/context/user/UserState.ts
@@ -1,1 +1,15 @@
 import React, { useReducer } from 'react';
+import axios from 'axios';
+import userReducer from './UserReducer';
+import userContext from './userContext';
+
+const userState = (props) => {
+  const userInfoFromStroage = JSON.parse(localStorage.getItem('userInfo') || '{}');
+
+  const initialState = {
+    userLogin: { userInfo: userInfoFromStorage },
+    loading: false,
+    error: null,
+  };
+  const [state, dispatch] = userReducer(userReducer, initialState);
+};
