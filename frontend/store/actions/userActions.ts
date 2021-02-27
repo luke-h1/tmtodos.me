87c081ab6a305f402270fb93d858b62b769d5ce3
luke-h1 <luke.howsam@yahoo.com>
2021-02-27 12:45:45 +0000

    user login w/redux working

--- frontend/store/actions/userActions.ts
+++ frontend/store/actions/userActions.ts
@@ -57,30 +57,30 @@ export const logut = () => (dispatch) => {
 };
 
 export const register = (name, email, password) => async (dispatch) => {
-    try {
-      dispatch({
-        type: USER_REGISTER_REQUEST,
-      });
-      const config = {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      };
-      const { data } = await axios.post(
-        "/api/users/",
-        { name, email, password },
-        config
-      );
-      dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
-      dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
-      localStorage.setItem("userInfo", JSON.stringify(data));
-    } catch (e) {
-      dispatch({
-        type: USER_REGISTER_FAIL,
-        payload:
+  try {
+    dispatch({
+      type: USER_REGISTER_REQUEST,
+    });
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    const { data } = await axios.post(
+      'http://localhost:5000/api/users/',
+      { name, email, password },
+      config,
+    );
+    dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
+    dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+    localStorage.setItem('userInfo', JSON.stringify(data));
+  } catch (e) {
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      payload:
           e.response && e.response.data.message
             ? e.response.data.message
             : e.message,
-      });
-    }
-  };
+    });
+  }
+};
