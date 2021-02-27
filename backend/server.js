7ad8c18dd8f14f733e4c9e9bf15070334d0d5255
luke-h1 <luke.howsam@yahoo.com>
2021-02-27 19:01:04 +0000

    add morgan

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
+import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
 import connectDB from './config/db.js';
@@ -19,6 +20,7 @@ const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
+app.use(morgan());
 
 app.use('/api/users', userRoutes);
 
