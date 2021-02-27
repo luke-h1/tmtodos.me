998668d018c01c112b1028f7423b3379c2ebd7a2
luke-h1 <luke.howsam@yahoo.com>
2021-02-27 13:06:37 +0000

    login functionality working

--- frontend/screens/LoginScreen.tsx
+++ frontend/screens/LoginScreen.tsx
@@ -10,50 +10,84 @@ import {
   Text,
   Center,
 } from '@chakra-ui/react';
-
+import { useDispatch, useSelector } from 'react-redux';
 import Loader from 'components/Loader';
 import { Button } from 'components/Button';
+import Error from 'components/Error';
+
+import { login } from '../store/actions/userActions';
 import { LoginSchema } from '../validations/userValidation';
 
-const LoginScreen = () => (
-  <>
-    <Flex direction="column" justify="center" align="center" minH={50} mb={10}>
-      <Box>
-        <Heading as="h1" fontSize="40px" mb={4}>
-          Login
-        </Heading>
-      </Box>
-    </Flex>
-    <Flex
-      direction="column"
-      justify="center"
-      align="center"
-      mx="auto"
-      maxW="660px"
-    >
-      <form>
-        <FormControl id="email">
-          <FormLabel>Email address</FormLabel>
-          <Input type="email" mb={8} />
-        </FormControl>
+const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-        <FormControl id="password">
-          <FormLabel>Password</FormLabel>
-          <Input type="password" />
-        </FormControl>
-      </form>
-      <Button>Login</Button>
-    </Flex>
-    <Center mt={10}>
-      <Box p="8" w="500px" borderWidth="1px" rounded="lg" flexBasis="45%">
-        <Heading as="h3" size="lg" mb="2">Not a user ? </Heading>
-        <Text fontSize="lg">Register today !</Text>
-        <Button><Link href='/register'><a>
-          Signup
-          </a></Link></Button>
-      </Box>
-    </Center>
+  const dispatch = useDispatch();
 
-  </>
-);
+  const userLogin = useSelector((state) => state.userLogin);
+  const { loading, error, userInfo } = userLogin;
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(login(email, password));
+  };
+  return (
+    <>
+      <Flex direction="column" justify="center" align="center" minH={50} mb={10}>
+        <Box>
+          <Heading as="h1" fontSize="40px" mb={4}>
+            Login
+          </Heading>
+        </Box>
+      </Flex>
+      {loading && <Loader />}
+      {error && <Error>{error}</Error>}
+
+      <Flex
+        direction="column"
+        justify="center"
+        align="center"
+        mx="auto"
+        maxW="660px"
+      >
+        <form onSubmit={submitHandler}>
+          <FormControl id="email">
+            <FormLabel>Email address</FormLabel>
+            <Input
+              type="email"
+              mb={8}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </FormControl>
+
+          <FormControl id="password">
+            <FormLabel>Password</FormLabel>
+            <Input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </FormControl>
+          <Button type="submit">Login</Button>
+        </form>
+
+      </Flex>
+      <Center mt={10}>
+        <Box p="8" w="500px" borderWidth="1px" rounded="lg" flexBasis="45%">
+          <Heading as="h3" size="lg" mb="2">Not a user ? </Heading>
+          <Text fontSize="lg">Register today !</Text>
+          <Button>
+            <Link href="/register">
+              <a>
+                Signup
+              </a>
+            </Link>
+          </Button>
+        </Box>
+      </Center>
+
+    </>
+  );
+};
 export default LoginScreen;
