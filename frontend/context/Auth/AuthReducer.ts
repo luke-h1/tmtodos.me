340ae074353f006929a17b63dab7d832ec4d9c3e
luke-h1 <luke.howsam@yahoo.com>
2021-02-27 11:34:04 +0000

    start to work on Authentication

--- frontend/context/Auth/AuthReducer.ts
+++ frontend/context/Auth/AuthReducer.ts
@@ -1,175 +1,56 @@
-import { USER_UPDATE_RESET } from 'constants/userConstants';
 import {
-  USER_DETAILS_FAIL,
-  USER_DETAILS_REQUEST,
-  USER_DETAILS_SUCCESS,
-  USER_LOGIN_FAIL,
-  USER_LOGIN_REQUEST,
-  USER_LOGIN_SUCCESS,
-  USER_LOGOUT,
-  USER_REGISTER_FAIL,
-  USER_REGISTER_REQUEST,
-  USER_REGISTER_SUCCESS,
-  USER_UPDATE_PROFILE_FAIL,
-  USER_UPDATE_PROFILE_REQUEST,
-  USER_UPDATE_PROFILE_SUCCESS,
-  USER_DETAILS_RESET,
-  USER_LIST_REQUEST,
-  USER_LIST_SUCCESS,
-  USER_LIST_FAIL,
-  USER_LIST_RESET,
-  USER_DELETE_REQUEST,
-  USER_DELETE_SUCCESS,
-  USER_DELETE_FAIL,
-  USER_UPDATE_REQUEST,
-  USER_UPDATE_SUCCESS,
-  USER_UPDATE_FAIL,
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
   CLEAR_ERRORS,
-} from '../../constants/userConstants';
+
+} from '../../constants/AuthConstants';
 
 export default (state, action) => {
   switch (action.type) {
-    default: {
-      return state;
-    }
-
-    case USER_REGISTER_REQUEST:
+    case USER_LOADED:
       return {
-        loading: true,
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: action.payload,
       };
 
-    case USER_REGISTER_SUCCESS:
-    case USER_LOGIN_SUCCESS: {
+    case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS: {
       localStorage.setItem('token', action.payload.token);
-      console.log(action.payload);
       return {
         ...state,
         ...action.payload,
         isAuthenticated: true,
         loading: false,
-        token: action.payload.token,
-        success: true,
-        user: action.payload,
       };
     }
-
-    case USER_REGISTER_FAIL:
-      return {
-        loading: false,
-        error: action.payload,
-      };
-
-    case USER_LOGIN_REQUEST:
-      return {
-        loading: true,
-      };
-
-    case USER_LOGIN_FAIL: {
-      localStorage.removeItem('token');
-      return {
-        ...state,
-        token: null,
-        isAuthenticated: false,
-        loading: false,
-        user: null,
-        error: action.payload,
-      };
-    }
-    case USER_LOGOUT:
-      localStorage.removeItem('token');
-      return {
-        ...state,
-        token: null,
-        isAuthenticated: false,
-        loading: false,
-        user: null,
-      };
-
     case CLEAR_ERRORS: {
       return {
         ...state,
         error: null,
       };
     }
-    case USER_DETAILS_FAIL: {
+    case REGISTER_FAIL:
+    case AUTH_ERROR:
+    case LOGIN_FAIL:
+    case LOGOUT:
+      localStorage.removeItem('token');
       return {
-        error: action.payload,
+        ...state,
+        token: null,
+        isAuthenticated: false,
         loading: false,
-      };
-    }
-    case USER_DETAILS_REQUEST: {
-      return {
-        loading: true,
-      };
-    }
-    case USER_DETAILS_SUCCESS: {
-      return {
-        success: true,
-        loading: false,
-        user: action.payload,
-      };
-    }
-    case USER_LIST_REQUEST: {
-      return {
-        loading: true,
-
-      };
-    }
-    case USER_LIST_SUCCESS: {
-      return {
-        loading: false,
-        success: true,
-        users: action.payload,
-      };
-    }
-    case USER_LIST_FAIL: {
-      return {
-        loading: false,
-        success: false,
-        users: [],
+        user: null,
         error: action.payload,
       };
+    default: {
+      return state;
     }
-    case USER_LIST_RESET: {
-      return {
-        users: [],
-        errors: action.payload,
-        loading: false,
-      };
-    }
-    case USER_UPDATE_PROFILE_FAIL: {
-      return { loading: false, error: action.payload, success: false };
-    }
-    case USER_UPDATE_PROFILE_REQUEST:
-      return { success: true, loading: true };
-
-    case USER_UPDATE_PROFILE_SUCCESS:
-      return { loading: false, success: true, user: action.payload };
-
-    case USER_DETAILS_RESET:
-      return { user: {} };
-
-    case USER_DELETE_REQUEST:
-      return { loading: true };
-
-    case USER_DELETE_SUCCESS:
-      return { loading: false, success: true };
-
-    case USER_DELETE_FAIL:
-      return { loading: false, error: action.payload };
-
-    case USER_UPDATE_REQUEST:
-      return { loading: true };
-
-    case USER_UPDATE_SUCCESS:
-      return { loading: false, success: true };
-
-    case USER_UPDATE_FAIL:
-      return { loading: false, error: action.payload };
-
-    case USER_UPDATE_RESET:
-      return {
-        user: {},
-      };
   }
 };
--- frontend/context/Auth/AuthState.tsx
+++ frontend/context/Auth/AuthState.tsx
@@ -3,252 +3,63 @@ import axios from 'axios';
 import AuthContext from './authContext';
 import authReducer from './AuthReducer';
 import setAuthToken from '../../utils/setAuthToken';
-import {
-  USER_DETAILS_FAIL,
-  USER_DETAILS_REQUEST,
-  USER_DETAILS_SUCCESS,
-  USER_LOGIN_FAIL,
-  USER_LOGIN_REQUEST,
-  USER_LOGIN_SUCCESS,
-  USER_LOGOUT,
-  USER_REGISTER_FAIL,
-  USER_REGISTER_REQUEST,
-  USER_REGISTER_SUCCESS,
-  USER_UPDATE_PROFILE_FAIL,
-  USER_UPDATE_PROFILE_REQUEST,
-  USER_UPDATE_PROFILE_SUCCESS,
-  USER_DETAILS_RESET,
-  USER_LIST_REQUEST,
-  USER_LIST_SUCCESS,
-  USER_LIST_FAIL,
-  USER_LIST_RESET,
-  USER_DELETE_REQUEST,
-  USER_DELETE_SUCCESS,
-  USER_DELETE_FAIL,
-  USER_UPDATE_REQUEST,
-  USER_UPDATE_SUCCESS,
-  USER_UPDATE_FAIL,
-  CLEAR_ERRORS,
-} from '../../constants/userConstants';
 
-const AuthState = ({ children }) => {
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  CLEAR_ERRORS,
+} from '../../constants/AuthConstants';
+
+// /api/users/profile
+const AuthState = (props) => {
   const initialState = {
-    // eslint-disable-next-line valid-typeof
-    token: typeof window !== 'undefined' ? localStorage.getItem('token') : '{}',
-    isAuthenticated: false,
-    loading: false,
+    token: typeof localStorage !== 'undefined' ? localStorage.getItem('token') : '{}',
+    isAuthenticated: null,
+    loading: true,
     error: null,
     user: null,
   };
 
   const [state, dispatch] = useReducer(authReducer, initialState);
 
-  // REGISTER A USER
-  const register = async (name: string, email: string, password: string) => {
+  // load current logged in user & get JWT token
+  const loadUser = async () => {
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
     try {
-      dispatch({
-        type: USER_REGISTER_REQUEST,
-      });
+      const res = await axios.get('http://localhost:5000/api/users/profile');
+      dispatch({ type: USER_LOADED, payload: res.data });
+    } catch (e) {
+      dispatch({ type: AUTH_ERROR });
+    }
+  };
+
+  const register = async (name, email, password) => {
+    try {
       const config = {
         headers: {
           'Content-Type': 'application/json',
         },
       };
-      const { data } = await axios.post('/api/users', { name, email, password }, config);
-      dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
-      dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+      const res = await axios.post('/api/users', { name, email, password }, config);
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data,
+      });
+      loadUser();
     } catch (e) {
       dispatch({
-        type: USER_REGISTER_FAIL,
-        payload:
-              e.response && e.response.data.message
-                ? e.response.data.message
-                : e.message,
+        type: REGISTER_FAIL,
+        payload: e.response && e.response.data.message
+          ? e.response.data.message
+          : e.message,
       });
     }
   };
-
-  const login = async (email, password) => {
-    try {
-      dispatch({
-        type: USER_LOGIN_REQUEST,
-      });
-      const config = {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      };
-      const { data } = await axios.post(
-        '/api/users/login',
-        { email, password },
-        config,
-      );
-      dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
-      localStorage.setItem('token', JSON.stringify(data));
-    } catch (e) {
-      dispatch({
-        type: USER_LOGIN_FAIL,
-        payload:
-              e.response && e.response.data.message
-                ? e.response.data.message
-                : e.message,
-      });
-    }
-  };
-
-  const logout = async () => {
-    localStorage.removeItem('token');
-    dispatch({ type: USER_LOGOUT });
-    dispatch({ type: USER_DETAILS_RESET });
-    dispatch({ type: USER_LIST_RESET });
-  };
-
-  const getUserDetails = async (id) => {
-    try {
-      dispatch({
-        type: USER_DETAILS_REQUEST,
-      });
-      const token = localStorage.getItem('token') && localStorage.getItem('token');
-      const config = {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`,
-        },
-      };
-      const { data } = await axios.get(`/api/users/${id}`, config);
-      dispatch({ type: USER_DETAILS_SUCCESS, payload: data });
-    } catch (e) {
-      dispatch({
-        type: USER_DETAILS_FAIL,
-        payload:
-              e.response && e.response.data.message
-                ? e.response.data.message
-                : e.message,
-      });
-    }
-  };
-
-  const updateUserProfile = async (user) => {
-    try {
-      dispatch({
-        type: USER_UPDATE_PROFILE_REQUEST,
-      });
-      const token = localStorage.getItem('token') && localStorage.getItem('token');
-      const config = {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`,
-        },
-      };
-      const { data } = await axios.put('/api/users/profile', user, config);
-      dispatch({ type: USER_UPDATE_PROFILE_SUCCESS, payload: data });
-    } catch (e) {
-      dispatch({
-        type: USER_UPDATE_PROFILE_FAIL,
-        payload:
-              e.response && e.response.data.message
-                ? e.response.data.message
-                : e.message,
-      });
-    }
-  };
-
-  const listUsers = async () => {
-    try {
-      dispatch({
-        type: USER_LIST_REQUEST,
-      });
-      const token = localStorage.getItem('token') && localStorage.getItem('token');
-      const config = {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      };
-      const { data } = await axios.get('/api/users', config);
-      dispatch({ type: USER_LIST_SUCCESS, payload: data });
-    } catch (e) {
-      dispatch({
-        type: USER_LIST_FAIL,
-        payload:
-              e.response && e.response.data.message
-                ? e.response.data.message
-                : e.message,
-      });
-    }
-  };
-  const deleteUser = async (id) => {
-    try {
-      dispatch({
-        type: USER_DELETE_REQUEST,
-      });
-      const token = localStorage.getItem('token') && localStorage.getItem('token');
-      const config = {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      };
-      await axios.delete(`/api/users/${id}`, config);
-      dispatch({ type: USER_DELETE_SUCCESS });
-    } catch (e) {
-      dispatch({
-        type: USER_DELETE_FAIL,
-        payload:
-              e.response && e.response.data.message
-                ? e.response.data.message
-                : e.message,
-      });
-    }
-  };
-
-  const updateUser = async (user) => {
-    try {
-      dispatch({
-        type: USER_UPDATE_REQUEST,
-
-      });
-      const token = localStorage.getItem('token') && localStorage.getItem('token');
-      const config = {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`,
-        },
-      };
-      const { data } = await axios.put(`/api/users/${user._id}`, user, config);
-      dispatch({ type: USER_UPDATE_SUCCESS });
-      dispatch({ type: USER_DETAILS_SUCCESS, payload: data });
-    } catch (e) {
-      dispatch({
-        type: USER_UPDATE_FAIL,
-        payload:
-              e.response && e.response.data.message
-                ? e.response.data.message
-                : e.message,
-      });
-    }
-  };
-  const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
-
-  return (
-    <AuthContext.Provider
-      value={{
-        token: state.token,
-        isAuthenticated: state.isAuthenticated,
-        loading: state.loading,
-        user: state.user,
-        error: state.error,
-        register,
-        login,
-        logout,
-        getUserDetails,
-        updateUserProfile,
-        listUsers,
-        deleteUser,
-        updateUser,
-        clearErrors,
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
 };
-export default AuthState;
