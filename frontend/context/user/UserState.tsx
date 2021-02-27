8c0e539ff5f7714581a89f9d8c7230bc413b9ff5
luke-h1 <luke.howsam@yahoo.com>
2021-02-27 14:23:13 +0000

    updates

--- frontend/context/user/UserState.tsx
+++ frontend/context/user/UserState.tsx
@@ -16,7 +16,6 @@ import {
 
 const UserState = ({ children }) => {
   const initialState = {
-
     userInfo: {
       token:
       typeof localStorage !== 'undefined'
@@ -25,90 +24,27 @@ const UserState = ({ children }) => {
       isAuthenticated: null,
       error: null,
     },
+    userDetails: {
       user: {},
-      users: [],
-    
+    },
+    users: [],
+    userList: [],
     loading: false,
 
   };
 
   const [state, dispatch] = useReducer(userReducer, initialState);
 
-  // LOAD USER (HIT AUTH ENDPOINT)
-  const loadUser = async () => {
-    if (localStorage.token) {
-      setAuthToken(localStorage.token);
-    }
-    try {
-      const res = await axios.get('/api/auth');
-      dispatch({ type: USER_LOADED, payload: res.data }); // USER DATA
-    } catch (err) {
-      console.log(err);
-      dispatch({ type: AUTH_ERROR });
-    }
-  };
 
-  // REGISTER USER
-  const register = async (formData) => {
-    const config = {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    };
-    try {
-      const res = await axios.post('/api/users', formData, config);
-      dispatch({
-        type: REGISTER_SUCCESS,
-        payload: res.data,
-      });
-      loadUser();
-    } catch (err) {
-      dispatch({
-        type: REGISTER_FAIL,
-        payload: err.response.data.msg,
-      });
-    }
-  };
-
-  // LOGIN USER
-  const login = async (formData) => {
-    const config = {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    };
-    try {
-      const res = await axios.post('/api/auth', formData, config);
-      dispatch({
-        type: LOGIN_SUCCESS,
-        payload: res.data,
-      });
-      loadUser();
-    } catch (err) {
-      dispatch({
-        type: LOGIN_FAIL,
-        payload: err.response.data.msg,
-      });
-    }
-  };
-
-  // LOGOUT (DESTROY TOKEN)
-  const logout = () => dispatch({ type: LOGOUT });
-
-  // CLEAR ERRORS (CLEAR ERRORS IN STATE)
-  const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
 
   return (
     <UserContext.Provider
       value={{
-        loading: state.loading,
-        error: state.error,
         userInfo: state.userInfo,
-        register,
-        loadUser,
-        login,
-        logout,
-        clearErrors,
+        userDetails: state.userDetails,
+        users: state.users,
+        userList: state.userList,
+        loading: state.loading,
       }}
     >
       {children}
