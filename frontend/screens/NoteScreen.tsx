4c114f368f7409695ceb5317aee025aee0d052db
luke-h1 <luke.howsam@yahoo.com>
2021-03-01 20:27:42 +0000

    list notes not working

--- frontend/screens/NoteScreen.tsx
+++ frontend/screens/NoteScreen.tsx
@@ -23,15 +23,20 @@ const NoteScreen = () => {
   const noteCreate = useSelector((state) => state.noteCreate);
   const noteList = useSelector((state) => state.noteList);
 
-  const { notes } = noteList;
+  const { loading: loadingList, errors, notes } = noteList;
 
   const { loading, error } = noteCreate;
 
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(createNote(title, body));
-    dispatch(listNotes());
   };
+
+  useEffect(() => {
+    if (notes && notes.length !== 0) {
+      dispatch(listNotes());
+    }
+  }, [loading]);
 
   return (
     <Center>
@@ -40,7 +45,7 @@ const NoteScreen = () => {
           Notes
         </Text>
         {error && <Error>{error}</Error>}
-        {loading && <Loader />}
+        {loading ? <Loader /> : loadingList && <Loader />}
 
         <Flex
           direction="column"
@@ -63,36 +68,11 @@ const NoteScreen = () => {
             name="body"
             value={body}
             onChange={(e) => setBody(e.target.value)}
-
           />
           <Button onClick={handleSubmit}>Submit</Button>
         </Flex>
         <Flex direction="column" justify="center" align="center">
-            {/* <>
-              <Box
-                shadow="sm"
-                rounded="md"
-                data-testid="card"
-                maxW="md"
-                minW="lg"
-                mt={4}
-                borderWidth="1px"
-                borderRadius="md"
-                overflow="hidden"
-                _hover={{ color: '#2EC0F9' }}
-              >
-                <Heading m="5" mb="2" as="h1" size="lg">
-                  {note.title}
-                </Heading>
-                <Text m="5" mt="2" mb="4">
-                  {note.body}
-                </Text>
-                <Text m="5" mt="2" mb="4">
-                  {format(parseISO(note.updatedAt), 'MMMM dd, yyyy')}
-                </Text>
-              </Box>
-            </> */}
-
+          {notes.length !== 0 && console.log(notes)}
         </Flex>
       </Flex>
     </Center>
