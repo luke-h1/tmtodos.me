3ba55754b066cc94bdcaea64cdb14f3c704d36da
luke-h1 <luke.howsam@yahoo.com>
2021-03-21 12:01:27 +0000

    feat/ui-improvs

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,9 @@
-import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
 import { NormalLink } from "../components/NormalLink";
 import { Spinner } from "../components/Spinner";
-import { Wrapper } from "../components/Wrapper";
 import { useNotesQuery } from "../generated/graphql";
 export const Home: React.FC<{}> = () => {
-  const { data } = useNotesQuery({ fetchPolicy: 'network-only' });
+  const { data } = useNotesQuery({ fetchPolicy: "network-only" });
 
   return (
     <div className="h-24 min-h-full mt-4">
@@ -15,20 +13,31 @@ export const Home: React.FC<{}> = () => {
       {!data ? (
         <Spinner />
       ) : (
-        <Wrapper>
+        <div className="flex flex-col items-center">
           <h1 className="text-xl">
             <NormalLink to="/notes">Create a note</NormalLink>
           </h1>
           {data.notes &&
             data.notes.map((i) => (
               <>
-                <div>title: {i.title}</div>
-                <div>text: {i.text}</div>
-                <hr />
-                <br />
+                <div className="shadow p-4 bg-white mt-4 mb-4  max-w-lg w-full">
+                  <div className="text-left">
+                    <h3 className="mb-2 text-gray-700">Title: {i.title}</h3>
+                    <p className="text-grey-600 text-sm">Body: {i.text}</p>
+                  </div>
+                  ​
+                  <div className="mt-4">
+                    <button className="bg-red-500 hover:bg-red-300 text-white font-bold py-2 px-4 rounded">
+                      Delete note
+                    </button>
+                    <button className="bg-blue-500 hover:bg-blue-300 text-white font-bold py-2 px-4 rounded ml-4">
+                      Edit note
+                    </button>
+                  </div>
+                </div>
               </>
             ))}
-        </Wrapper>
+        </div>
       )}
     </div>
   );
