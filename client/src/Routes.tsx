75f8ad9b0da8a71bb053c57d198da6f7f3a2159b
luke-h1 <luke.howsam@yahoo.com>
2021-03-21 11:37:21 +0000

    feat/fix-notes

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -4,6 +4,7 @@ import { Login } from "./pages/Login";
 import { Home } from "./pages/Home";
 import { Navbar } from "./components/Navbar";
 import { Register } from "./pages/Register";
+import { Notes } from "./pages/Notes";
 
 export const Routes: React.FC = () => {
   return (
@@ -13,6 +14,8 @@ export const Routes: React.FC = () => {
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
+        <Route exact path="/notes" component={Notes} />
+
       </Switch>
     </Router>
   );
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,17 +1,25 @@
-import React, { useEffect } from "react";
+import React from "react";
+import { Wrapper } from "../components/Wrapper";
 import { useNotesQuery } from "../generated/graphql";
 export const Home: React.FC<{}> = () => {
-  // const [{ data }] = useNotesQuery();
+  const { data } = useNotesQuery();
   return (
     <div className="h-24 min-h-full mt-4">
       <h1 className="min-h-full font-body align-center text-4xl text-center">
         Take My Notes
       </h1>
-      {/* {!data ? (
+      {!data ? (
         <div>loading...</div>
       ) : (
-        data.notes.map((p: any) => <div key={p.id}>{p.title}</div>)
-      )} */}
+        data.notes.map((p) => (
+          <Wrapper>
+            <div>title: {p.title}</div>
+            <div>text: {p.text}</div>
+            <hr />
+            <br />
+          </Wrapper>
+        ))
+      )}
     </div>
   );
 };
