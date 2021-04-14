0ccc59cb4c4bab47a62c0f57b0a14c53793e9c7b
luke-h1 <luke.howsam@yahoo.com>
2021-04-14 19:50:11 +0100

    feat/rename-todo - rename docker images

--- server/deploy.sh
+++ server/deploy.sh
@@ -6,6 +6,6 @@ read VERSION
 
 echo "Deploying to production 🚀 🔥"
 
-docker build -t lhowsam/todos:$VERSION
-docker push lhowsam/todos:$VERSION
-ssh root@DevVPS "docker pull lhowsam/todos:$VERSION && docker tag lhowsam/todos:$VERSION dokku/api:$VERSION && dokku deploy api $VERSION"
+docker build -t lhowsam/tmtodosme:$VERSION
+docker push lhowsam/tmtodosme:$VERSION
+ssh root@DevVPS "docker pull lhowsam/tmtodosme:$VERSION && docker tag lhowsam/tmtodosme:$VERSION dokku/api:$VERSION && dokku deploy api $VERSION"
