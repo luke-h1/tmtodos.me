95512a31f7f445a1c09a54b3462e2915795416af
luke-h1 <luke.howsam@yahoo.com>
2021-04-17 20:12:00 +0100

    fix seo config

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -13,24 +13,23 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           />
 
-          <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
-          <link href="/static/favicons/site.webmanifest" rel="manifest" />
+          <link href="/favicon.ico" rel="shortcut icon" />
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/static/favicons/apple-touch-icon.png"
+            href="/apple-touch-icon.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/static/favicons/favicon-32x32.png"
+            href="/favicon-32x32.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/static/favicons/favicon-16x16.png"
+            href="/favicon-16x16.png"
           />
           <link rel="manifest" href="/site.webmanifest" />
           <meta name="theme-color" content="#ffffff" />
