e2fa47d08b5ef4b3351aee23111737af177f3f37
luke-h1 <luke.howsam@yahoo.com>
2021-04-17 20:48:49 +0100

    feat/update-single-todo-styles

--- client/src/pages/todo/[id].tsx
+++ client/src/pages/todo/[id].tsx
@@ -7,8 +7,10 @@ import { withUrqlClient } from 'next-urql';
 import React from 'react';
 import { createUrqlClient } from 'utils/createUrqlClient';
 import { useGetTodoFromUrl } from 'utils/useGetTodoFromUrl';
+// import { parseISO, format } from 'date-fns';
 
-interface SingleTodoProps {}
+interface SingleTodoProps {
+}
 
 const SingleTodo: React.FC<SingleTodoProps> = () => {
   const [{ data, error, fetching }] = useGetTodoFromUrl();
@@ -33,25 +35,36 @@ const SingleTodo: React.FC<SingleTodoProps> = () => {
   }
   return (
     <>
-      <CustomHead
-        title={`todo - ${data.todo.title} | tmtodos.me`}
-        description={`view your todo - ${data.todo.title}`}
-      />
-      <NextSeo
-        title={`todo - ${data.todo.title} | tmtodos.me`}
-        canonical={`https://tmtodos.me/todo/${data.todo.id}`}
-        openGraph={{
-          url: `https://tmtodos.me/todo/${data.todo.id}`,
-          title: `todo - ${data.todo.title}`,
-        }}
-      />
       <Flex>
-        <h1 className="text-center text-2xl">{data?.todo?.title}</h1>
-        <div className="my-4 text-1xl">{data?.todo?.text}</div>
-        <EditDeleteTodoButtons
-          id={data.todo.id}
-          creatorId={data.todo.creator.id}
+        <CustomHead
+          title={`todo - ${data.todo.title} | tmtodos.me`}
+          description={`view your todo - ${data.todo.title}`}
         />
+        <NextSeo
+          title={`todo - ${data.todo.title} | tmtodos.me`}
+          canonical={`https://tmtodos.me/todo/${data.todo.id}`}
+          openGraph={{
+            url: `https://tmtodos.me/todo/${data.todo.id}`,
+            title: `todo - ${data.todo.title}`,
+          }}
+        />
+        <div className="flex flex-col justify-center mb-5">
+          <h1 className="text-3xl dark:text-gray-300">{data?.todo?.title}</h1>
+        </div>
+        <div className="flex flex-col justify-between mt-2 mb-5" />
+
+        <hr />
+        <div className="prose dark:prose-dark max-w-md w-full">
+          <p className="text-md mt-4 mb-4 leading-10 tracking-wider dark:text-gray-300 prose dark:prose-dark max-w-none w-full">
+            {data?.todo?.text}
+          </p>
+        </div>
+        <div className="flex flex-col align-center items-center">
+          <EditDeleteTodoButtons
+            id={data.todo.id}
+            creatorId={data.todo.creator.id}
+          />
+        </div>
       </Flex>
     </>
   );
