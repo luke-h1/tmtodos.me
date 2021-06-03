ec1a34ba046b39c39f3169b39d3b4bce732b5ac8
luke-h1 <luke.howsam@yahoo.com>
2021-06-04 00:26:23 +0100

    add client deploy script

---
+++ scripts/deploy:client.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd src/tm-web
+npm run format
+npm run lint 
+npm run tsc
+npm run cy:ci 
+npm run build 
+echo "CHECKS PASSED"
+vc -f 
+echo "Deployed to development ✅"
+echo "deploying to production"
+vc -f --prod
+echo "SUCCESFULLY DEPLOYED TO PRODUCTION ✅"
--- scripts/deploy:server.sh
+++ scripts/deploy:server.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo What should the version be ? 
 read VERSION 
 cd ../src/tm-server
