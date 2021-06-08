97294c17dc1a889e9dc42d6d6741e8ead85b083e
luke-h1 <luke.howsam@yahoo.com>
2021-06-08 23:37:06 +0100

    fix/deploy-errors

--- scripts/deploy:server.sh
+++ scripts/deploy:server.sh
@@ -3,9 +3,11 @@ echo What should the version be ?
 read VERSION
 echo Enter IP address
 read TARGET
+echo Enter user 
+read USER 
 cd ../src/tm-server
 echo "Deploying to production 🚀 🔥"
 
 docker build -t lhowsam/tmtodos:$VERSION .
 docker push lhowsam/tmtodos:$VERSION
-ssh ubuntu@${TARGET} -i /Users/lukehowsam/aws/*.cer "sudo docker pull lhowsam/tmtodos:$VERSION && sudo docker tag lhowsam/tmtodos:$VERSION dokku/tm-api:$VERSION && dokku repo:purge-cache tm-api && dokku deploy tm-api $VERSION"
+ssh ${USER}@${TARGET} -i /Users/lukehowsam/aws/*.cer "sudo docker pull lhowsam/tmtodos:$VERSION && sudo docker tag lhowsam/tmtodos:$VERSION dokku/tm-api:$VERSION tm-api && dokku deploy tm-api $VERSION"
