fcd18c3d474fe1cadc7d7b30a26cf3f92d1b7aa5
luke-h1 <luke.howsam@yahoo.com>
2021-06-13 15:17:03 +0100

    disable playground on production

--- scripts/deploy:server.sh
+++ scripts/deploy:server.sh
@@ -7,7 +7,6 @@ echo Enter user
 read USER 
 cd ../src/tm-server
 echo "Deploying to production 🚀 🔥"
-
 docker build -t lhowsam/tmtodos:$VERSION .
 docker push lhowsam/tmtodos:$VERSION
 ssh ${USER}@${TARGET} -i /Users/lukehowsam/aws/*.cer "sudo docker pull lhowsam/tmtodos:$VERSION && sudo docker tag lhowsam/tmtodos:$VERSION dokku/tm-api:$VERSION && dokku deploy tm-api $VERSION"
