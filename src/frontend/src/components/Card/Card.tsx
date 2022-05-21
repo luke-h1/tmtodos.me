e656c9a887092f47917bb74f835255eee1b1f0f7
luke-h1 <luke.howsam@yahoo.com>
2022-05-21 12:55:56 +0100

    add card comp

---
+++ src/frontend/src/components/Card/Card.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { CardHeader, CardWrapper, CardBody } from './Card.styles';
+import { CardComposition, CardProps } from './Card.types';
+import { isHeaderElement } from './utils';
+
+function Card<T>(props: CardProps & CardComposition & T) {
+  const { as: Component, children, glass, depth, title, ...rest } = props;
+
+  const hasHeaderChildren = React.Children.toArray(children).some(child =>
+    isHeaderElement(child),
+  );
+
+  let header = null;
+
+  /**
+   * If no CardHeader children are present AND if a title has been specified as a prop,
+   * create an instance of CardHeader with the title prop
+   */
+
+  if (!hasHeaderChildren && title) {
+    header = <CardHeader>{title}</CardHeader>;
+  }
+
+  return (
+    <CardWrapper as={Component} depth={depth} glass={glass} {...rest}>
+      {header}
+      {children}
+    </CardWrapper>
+  );
+}
+Card.Header = CardHeader;
+Card.Body = CardBody;
+Card.DisplayName = 'Card';
