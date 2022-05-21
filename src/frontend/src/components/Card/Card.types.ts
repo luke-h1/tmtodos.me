431bc30513dcf33e0008eb9ff4f646c773cece94
luke-h1 <luke.howsam@yahoo.com>
2022-05-21 12:45:46 +0100

    add card.types.ts

---
+++ src/frontend/src/components/Card/Card.types.ts
@@ -0,0 +1,18 @@
+import { ElementType } from 'react';
+
+export interface CardWrapperProps {
+  glass?: boolean;
+  depth?: 0 | 1 | 2 | 3;
+}
+
+export interface CardComposition {
+  Header?: React.FC<{ className?: string }>;
+  Body?: React.FC<{ className?: string }>;
+}
+
+export interface CardProps extends CardWrapperProps {
+  title?: string;
+  className?: string;
+  as?: ElementType;
+  children?: React.ReactNode;
+}
